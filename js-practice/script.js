2f56bfadca98e2d319d329ddedc8c03924b476c1
deepan251999 <deepan251999@gmail,com>
2022-11-01 22:06:21 +0530

    js-practice.

---
+++ js-practice/script.js
@@ -0,0 +1,21 @@
+// post prefix
+
+
+// let a=5,b;
+// ++a;
+// console.log(a);
+
+// b=a++;
+// console.log(b);
+
+
+let ary=[2,5,3,44,6,77,9,18];
+
+
+let value=ary.map(item=>item*2);
+console.log(ary);
+console.log(value);
+
+let vote=ary.filter(age=> age>18);
+console.log(ary);
+console.log(vote);
